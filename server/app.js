388600bf92524771a2a43fbbd101b905d366f3f2
Jerel <jerelwilliams94@aol.com>
2018-09-23 23:09:47 -0400

    played around with server side

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const http2 = require('http2');
-const morgan = require('morgan');
-const { PerformanceObserver, performance } = require('perf_hooks');
+import express from 'express';
+import http2 from 'http2';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import path from 'path';
+import { PerformanceObserver, performance } from 'perf_hooks';
 
 const app = express();
-app.use(morgan('combined'));
+
+// Server =================================
 
 const port = process.env.PORT || 5000;
 
@@ -21,6 +24,14 @@ const obs = new PerformanceObserver(items => {
 server.listen(port, () => {
   console.log("server up");
 });
+
+// Middleware =============================
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes =================================
 
 app.get('/', (req, res) => {
   res.send('index page');
